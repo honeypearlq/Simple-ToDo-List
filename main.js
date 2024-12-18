2739959f97f8fb6ccfedb60ff444e44df02083b7
Honey Pearl Quinto <honeypearlq@gmail.com>
2024-12-18 22:00:21 +0800

    latest

--- main.js
+++ main.js
@@ -2,24 +2,16 @@ const toDoInput = document.querySelector('.todo-input');
 const toDoBtn = document.querySelector('.todo-btn');
 const toDoList = document.querySelector('.todo-list');
 
-// Theme Handlers
-const themeSelectors = document.querySelectorAll('.theme-selector');
-
-themeSelectors.forEach((selector) => {
-  selector.addEventListener('click', () => {
-    const theme = selector.classList[1].split('-')[0];
-    document.body.style.backgroundColor = theme === 'pink' ? '#FFCCE1' : theme === 'yellow' ? '#FFF5D7' : '#F2F9FF';
-  });
-});
-
 // Add ToDo
 toDoBtn.addEventListener('click', (e) => {
   e.preventDefault();
+  if (toDoInput.value.trim() === "") return;
+
   const toDoDiv = document.createElement('div');
   toDoDiv.classList.add('todo');
 
   const newToDo = document.createElement('li');
-  newToDo.innerText = toDoInput.value || "Empty Task";
+  newToDo.innerText = toDoInput.value;
   newToDo.classList.add('todo-item');
   toDoDiv.appendChild(newToDo);
 
@@ -39,3 +31,23 @@ toDoBtn.addEventListener('click', (e) => {
   checkBtn.addEventListener('click', () => toDoDiv.classList.toggle('completed'));
   deleteBtn.addEventListener('click', () => toDoDiv.remove());
 });
+
+// Theme Handlers
+const themeSelectors = document.querySelectorAll('.theme-selector');
+
+themeSelectors.forEach((selector) => {
+  selector.addEventListener('click', () => {
+    const theme = selector.classList[1].split('-')[0];
+    document.body.style.backgroundColor = theme === 'pink' ? '#FFCCE1' : theme === 'yellow' ? '#FFF5D7' : '#F2F9FF';
+  });
+});
+
+// Time and Date
+const dateTime = document.getElementById('datetime');
+function updateTime() {
+  const now = new Date();
+  const options = { weekday: 'long', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+  dateTime.innerText = now.toLocaleDateString('en-US', options);
+}
+setInterval(updateTime, 1000);
+updateTime();
